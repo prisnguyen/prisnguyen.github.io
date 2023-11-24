284cf8debb62451d60a78dc5a59312e12b077d8c
Priscilla N <pnguyn@bu.edu>
2023-11-24 17:00:22 -0500

    add about

--- components/js/script.js
+++ components/js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["a CS enthusiast", "a travel addict", "a meditator"],
+    strings: ["a CS enthusiast", "a developer", "a travel addict", "a meditator"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
