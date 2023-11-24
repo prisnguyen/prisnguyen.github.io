4515d8aeaef6e5c202f720313fece75ea9cd971a
Priscilla N <pnguyn@bu.edu>
2023-11-24 04:32:40 -0500

    updating

--- components/js/script.js
+++ components/js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".text", {
-    strings: ["Frontend Developer", "Travel Addict", "Food Lover"],
+    strings: ["a CS enthusiast", "a travel addict", "a meditator"],
     typeSpeed: 100,
     backSpeed: 100,
     backDelay: 1000,
