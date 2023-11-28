40aa55ed03f116e92216ac82157e9b1d28182d67
Priscilla N <pnguyn@bu.edu>
2023-11-27 23:26:44 -0500

    add writing

--- components/js/scroll.js
+++ components/js/scroll.js
@@ -1,22 +1,11 @@
-$(function() {  
-  var scroll = $(document).scrollTop();
-  var navHeight = $('.header').outerHeight();
+window.onscroll = function() {scrollFunction()};
 
-  $(windown).scroll(function() {
-    var scroll = $(document).scrollTop();
-
-    if (scrolled > navHeight) {
-      $('.header').addClass('fixed-header');
-    }
-    else {
-      $('.header').removeClass('fixed-header');
-    }
-
-    if (scrolled > scroll) {
-      $('.header').removeClass('sticky');
-    } else {
-      $('.header').addClass('sticky');
-    }
-    scroll = $(document).scrollTop;
-  });
-});
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("header").style.padding = "30px 10px";
+    document.getElementById(".logo").style.fontSize = "25px";
+  } else {
+    document.getElementById("header").style.padding = "80px 10px";
+    document.getElementById(".logo").style.fontSize = "35px";
+  }
+}
